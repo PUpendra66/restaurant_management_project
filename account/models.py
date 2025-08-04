16d6172afc0b157c840269ec6343ec1e9debdcac
pullari upendra <168459590+PUpendra66@users.noreply.github.com>
2025-08-04 20:45:17 +0530

    17420

--- account/models.py
+++ account/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Restaurant(models.Model):
+    name=models.CharField(max_length=100,varbose_name="Restaurant Name")
+    owner_name=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    phone_number=models.CharField(max_length=20)
+    address=models.CharField(max_length=200)
+    city=models.CharField(max_length=100)
+    created_at=models.DateTimeField(auto_now_add=True)
 
+
+    def __str__(self):
+        return self.name
+
+        
 class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
