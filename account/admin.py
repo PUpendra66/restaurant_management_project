d6ab67a7d95cb2758126386195fcb58ae1798b04
pullari upendra <168459590+PUpendra66@users.noreply.github.com>
2025-08-06 20:15:36 +0530

    17424

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Menu,Order
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display=('name','price')
+    search_fields=('name',)
+
+@admin.register(Order)
+class OredrAdmin(admin.ModelAdmin):
+    list_display=('id','customer','status','total_amount','created_at')
+    list_filter=('status')
+    search_fields=('customer__username',)
+    filter_horizontal=('items',)
